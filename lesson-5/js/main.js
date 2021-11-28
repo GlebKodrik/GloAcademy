c4990b5342299955c4be056330719a5505815371
Gleb Kodrik <kodrikgleb2@mail.ru>
2021-11-29 01:12:20 +0300

    add slider & mixin

--- lesson-5/js/main.js
+++ lesson-5/js/main.js
@@ -1,13 +1,30 @@
-const swiper = new Swiper(".swiper", {
+const hotelSlider = new Swiper(".hotel-slider", {
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button-next",
-    prevEl: ".slider-button-prev",
+    nextEl: ".hotel-slider__button-next",
+    prevEl: ".hotel-slider__button-prev",
   },
   effect: "coverflow",
   // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button-next",
+    prevEl: ".reviews-slider__button-prev",
+  },
   scrollbar: {
     el: ".swiper-scrollbar",
   },
