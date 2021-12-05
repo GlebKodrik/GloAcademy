88c9b923c0a9d00bee0ea31b492d66d4efcdfcf6
Gleb Kodrik <kodrikgleb2@mail.ru>
2021-12-05 23:16:04 +0300

    add modal

--- lesson-5/js/main.js
+++ lesson-5/js/main.js
@@ -1,40 +1,72 @@
-const hotelSlider = new Swiper(".hotel-slider", {
-  loop: true,
+$(document).ready(function () {
+  const hotelSlider = new Swiper(".hotel-slider", {
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button-next",
-    prevEl: ".hotel-slider__button-prev",
-  },
-  effect: "coverflow",
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button-next",
+      prevEl: ".hotel-slider__button-prev",
+    },
+    effect: "coverflow",
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 
-const reviewsSlider = new Swiper(".reviews-slider", {
-  loop: true,
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button-next",
-    prevEl: ".reviews-slider__button-prev",
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button-next",
+      prevEl: ".reviews-slider__button-prev",
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 
-let menuBottom = document.querySelector(`.menu-button`);
-menuBottom.addEventListener("click", () => {
-  document.querySelector(`.header-menu`).classList.toggle("header-menu_active");
+  let menuBottom = document.querySelector(`.menu-button`);
+
+  menuBottom.addEventListener("click", () => {
+    document
+      .querySelector(`.header-menu`)
+      .classList.toggle("header-menu_active");
+  });
+
+  let modalButton = $("[data-toggle=modal]");
+  let closeModalButton = $(".modal__close");
+  const openOverlay = () => {
+    let modalOverlay = $(".modal__overlay");
+    let modalDialog = $(".modal__dialog");
+
+    modalOverlay.addClass("modal__overlay_visible");
+    modalDialog.addClass("modal__dialog_visible");
+  };
+  const closeOverlay = (event) => {
+    event && event.preventDefault();
+    let modalOverlay = $(".modal__overlay");
+    let modalDialog = $(".modal__dialog");
+
+    modalOverlay.removeClass("modal__overlay_visible");
+    modalDialog.removeClass("modal__dialog_visible");
+  };
+
+  modalButton.on("click", openOverlay);
+  closeModalButton.on("click", closeOverlay);
+
+  $(document).keyup(function (e) {
+    if (e.keyCode == 27) {
+      closeOverlay();
+    }
+  });
 });
